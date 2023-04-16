584eed52ab933e96f40bf207e7f74929bd330604
Kusik Ilya <kusikillya2001@gmail.com>
2023-04-16 18:36:04 +0300

    Add file upload

---
+++ src/GameDock/Server/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameDock.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public FileController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Index(IFormFile file)
+    {
+        var filePath = Path.Combine(_environment.ContentRootPath, "uploads", file.FileName);
+        await using var stream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(stream);
+        
+        return Ok();
+    }
+}
